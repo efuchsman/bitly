341caf5450df29ff1357da351edbfa22b8dd8b46
Eli Fuchsman <efuchsman@gmail.com>
2023-04-22 13:06:40 -0600

    Test decode model

--- app/models/decode.rb
+++ app/models/decode.rb
@@ -1,3 +1,7 @@
 class Decode < ApplicationRecord
-
+  validates_presence_of :bitlink
+  validates_presence_of :user_agent
+  validates_presence_of :timestamp
+  validates_presence_of :referrer
+  validates_presence_of :remote_ip
 end
