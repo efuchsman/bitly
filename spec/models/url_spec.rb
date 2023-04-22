6c4ac028a7a8b23f539c879260d17b8f25521235
Eli Fuchsman <efuchsman@gmail.com>
2023-04-22 14:06:18 -0600

    set up validations

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
+  describe "Relationships" do
+    it { should have_many :decodes}
+
+  end
+
   describe "Validations" do
     it { should validate_presence_of :long_url }
     it { should validate_presence_of :domain }
